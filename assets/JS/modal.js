c9ea5bb73cdab06790b1336d4887f635da86e95a
luksouza <apolloboficial@gmail.com>
2022-06-15 18:20:28 -0300

    ...

--- assets/JS/modal.js
+++ assets/JS/modal.js
@@ -4,20 +4,19 @@ const btnClose = document.querySelector('#closeModal')
 
 btnClose.addEventListener('click', ({ target }) => {
     modal.classList.add('disable')
-    modal.addEventListener('click', () =>{
-        modal.classList.add('disable')
-    })
     console.log('ola')
 })
 
-const modalOrcamento = () => {
-    modal.classList.toggle('disable')
-    window.scrollTo(0, 0)
+const modalOrcamento = ({ target }) => {
+    if(target.getAttribute('data-block') != null){
+        modal.classList.toggle('disable')
+        window.scrollTo(0, 0)
+    }
 }
 
 btnOrcamento.forEach((btn) => {
     btn.addEventListener('click', modalOrcamento)
-
+    modal.addEventListener('click', modalOrcamento)
 })
 
 
